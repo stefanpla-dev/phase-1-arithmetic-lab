cede4b765ca34a2e19f57bb9782de5c60026b160
Stefan Pla <spla@icloud.com>
2024-01-30 10:20:54 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+ let num1 = 31
+ let num2 = 2
+ let multiply = num1*num2;
+
+ let random = Math.round(Math.random());
+
+ let num3 = 24
+ let num4 = 5;
+ let mod = num3%num4;
+
+let num5 = 20
+
+let max = Math.max(num5, num2, num4);
+ 
